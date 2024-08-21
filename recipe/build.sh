716c8d6dcb9a88a8d172691261daa8c89afa8d5b
Justin Carpentier <justin.carpentier@inria.fr>
2024-08-21 13:55:00 +0200

    recipe: fix build for cross compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,12 +3,32 @@
 mkdir build
 cd build
 
+export BUILD_NUMPY_INCLUDE_DIRS=$( $PYTHON -c "import numpy; print (numpy.get_include())")
+export TARGET_NUMPY_INCLUDE_DIRS=$SP_DIR/numpy/core/include
+
+echo $BUILD_NUMPY_INCLUDE_DIRS
+echo $TARGET_NUMPY_INCLUDE_DIRS
+
+if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  echo "Copying files from $BUILD_NUMPY_INCLUDE_DIRS to $TARGET_NUMPY_INCLUDE_DIRS"
+  mkdir -p $TARGET_NUMPY_INCLUDE_DIRS
+  cp -r $BUILD_NUMPY_INCLUDE_DIRS/numpy $TARGET_NUMPY_INCLUDE_DIRS
+  export GENERATE_PYTHON_STUBS=0
+fi
+
 cmake .. \
       ${CMAKE_ARGS} \
       -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR=lib
+      -DPython3_NumPy_INCLUDE_DIR=$TARGET_NUMPY_INCLUDE_DIRS \
+      -DBUILD_TESTING=OFF \
+      -DPYTHON_EXECUTABLE=$PYTHON
 
 make -j${CPU_COUNT}
 make install
+
+if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  echo $BUILD_PREFIX
+  echo $PREFIX
+  sed -i.back 's|'"$BUILD_PREFIX"'|'"$PREFIX"'|g' $PREFIX/lib/cmake/pinocchio/pinocchioTargets.cmake
+  rm $PREFIX/lib/cmake/pinocchio/pinocchioTargets.cmake.back
+fi
