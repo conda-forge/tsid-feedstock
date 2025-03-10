433cc471c9a5a1679f3e98b06a2d98d27c140d75
Joris Vaillant <joris.vaillant@inria.fr>
2025-03-10 17:19:29 +0100

    Add cross build python

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,21 @@
 mkdir build
 cd build
 
+export BUILD_NUMPY_INCLUDE_DIRS=$( $PYTHON -c "import numpy; print (numpy.get_include())")
+export TARGET_NUMPY_INCLUDE_DIRS=$SP_DIR/numpy/core/include
+
+echo $BUILD_NUMPY_INCLUDE_DIRS
+echo $TARGET_NUMPY_INCLUDE_DIRS
+
+if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  echo "Copying files from $BUILD_NUMPY_INCLUDE_DIRS to $TARGET_NUMPY_INCLUDE_DIRS"
+  mkdir -p $TARGET_NUMPY_INCLUDE_DIRS
+  cp -r $BUILD_NUMPY_INCLUDE_DIRS/numpy $TARGET_NUMPY_INCLUDE_DIRS
+  export Python3_NumPy_INCLUDE_DIR=$TARGET_NUMPY_INCLUDE_DIRS
+else
+  export Python3_NumPy_INCLUDE_DIR=$BUILD_NUMPY_INCLUDE_DIRS
+fi
+
 cmake .. \
       ${CMAKE_ARGS} \
       -G"Ninja" \
@@ -11,7 +26,15 @@ cmake .. \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=lib \
       -DBUILD_TESTING=OFF \
-      -DPYTHON_EXECUTABLE=$PYTHON
+      -DPYTHON_EXECUTABLE=$PYTHON \
+      -DPython3_NumPy_INCLUDE_DIR=$Python3_NumPy_INCLUDE_DIR \
 
 ninja
 ninja install
+
+if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  echo $BUILD_PREFIX
+  echo $PREFIX
+  sed -i.back 's|'"$BUILD_PREFIX"'|'"$PREFIX"'|g' $PREFIX/lib/cmake/tsid/tsidTargets.cmake
+  rm $PREFIX/lib/cmake/tsid/tsidTargets.cmake.back
+fi
